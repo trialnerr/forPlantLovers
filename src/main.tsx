ee76b5ce35a3f441575f29fee7541978030acf98
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-16 15:45:08 -0500

    Implement routing with react-router and with App on index path and also added errorPage in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./routes/App.tsx";
+import { ErrorPage } from "./error-page.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
