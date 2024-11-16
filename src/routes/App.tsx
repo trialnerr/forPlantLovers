cdc9a2ce3bd27d0ee282168b91b2284529f2ebe1
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-16 15:49:47 -0500

    move App.tsx to routes folder

---
+++ src/routes/App.tsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import Main from "../components/Main";
+import NavBar from "../components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
