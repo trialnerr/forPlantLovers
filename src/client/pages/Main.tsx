2c543eaf33912ff72f9d7a0b0f0c51289e021f0c
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-18 00:17:45 -0500

    move pages into client

---
+++ src/client/pages/Main.tsx
@@ -0,0 +1,33 @@
+import Feature from "../components/Feature";
+import { FeatureProps } from "../../types";
+
+const Main = () => {
+  const feats: FeatureProps[] = [
+    { heading: "Feature 1", text: "i am feature 1 text, and i do things" },
+    { heading: "Feature 2", text: "i am feature 1 text, and i do things" },
+    { heading: "Feature 3", text: "i am feature 1 text, and i do things" },
+  ];
+
+  return (
+    <main className="relative isolate px-6 pt-6 lg:px-8 content-center min-h-[calc(100vh-2rem)]">
+      <div className="mx-auto max-w-3xl">
+        <div className="text-center">
+          <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+            Become a better plant parent.
+          </h1>
+          <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+            Learn how to take better care of your plant babies by using some of
+            the features we offer.
+          </p>
+        </div>
+        <section className="flex gap-4 ">
+          {feats.map((obj) => (
+            <Feature text={obj.text} heading={obj.heading} />
+          ))}
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
