3f3cac1b3dfc63c884a3216f81ea2af93bbbaf5d
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-18 00:18:20 -0500

    move main.tsx, index.css and error page into client folder

---
+++ src/client/main.tsx
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import { ErrorPage } from "./error-page.tsx";
+import RootLayout from "./layouts/RootLayout.tsx";
+import Main from "./pages/Main.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Main />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
