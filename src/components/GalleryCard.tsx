b2782eab041796a356866ccefa6d804d8f7e9e8a
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-18 19:50:07 -0500

    Delete unused lines

--- src/components/GalleryCard.tsx
+++ src/components/GalleryCard.tsx
@@ -1,6 +1,8 @@
 import { plantProps } from "../types";
 
-function GalleryCard({ common_name, image_url } : plantProps)  {
+function GalleryCard({ common_name, image_url }: plantProps) {
+  // const [name, setName] = useState(''); 
+  
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <img
