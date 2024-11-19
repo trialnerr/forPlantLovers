88e71433be14ac09e5bba0113fdcb3056d06877e
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-18 22:29:33 -0500

    remove dark mode styling. Will update when I decide on site colors

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,9 +1,9 @@
 function Upload() {
   return (
-    <div className="flex items-center justify-center w-full">
+    <div className="flex flex-col items-center justify-center w-full">
       <label
         htmlFor="dropzone-file"
-        className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+        className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
       >
         <div className="flex flex-col items-center justify-center pt-5 pb-6">
           <svg
@@ -21,17 +21,33 @@ function Upload() {
               d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
             />
           </svg>
-          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-            <span className="font-semibold">Click to upload</span> or drag and drop
+          <p className="mb-2 text-sm">
+            <span className="font-semibold">Click to upload</span>
           </p>
           <p className="text-xs text-gray-500 dark:text-gray-400">
-            SVG, PNG, JPG or GIF (MAX. 800x400px)
+            PNG, JPG, MAX:50MB
           </p>
         </div>
         <input id="dropzone-file" type="file" className="hidden" />
       </label>
+      <label
+        htmlFor="organSelect"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      >
+        Select an option
+      </label>
+      <select
+        id="organSelect"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      >
+        <option selected>Choose an organ</option>
+        <option value="leaf">LEAF</option>
+        <option value="bark">BARK</option>
+        <option value="flower">FLOWER</option>
+        <option value="fruit">FRUIT</option>
+      </select>
     </div>
   );
 }
 
-export default Upload
+export default Upload;
