9903796582b8f7a1e9837cc5180e7f8ae706b5d0
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-18 21:30:39 -0500

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
-const express = require('express'); 
-require("dotenv").config();
-const path = require('path'); 
+const express = require("express");
+const path = require("path");
+const axios = require("axios");
+const apiReq = require('./apiTest'); 
+
+// apiReq();  (dont use! Limited credits)
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,11 +11,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.get("/api", (req, res) => {
-  res.json(['banana', 'apple']);
+  res.json(["banana", "apple"]);
 });
 
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
+
