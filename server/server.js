3e7632d87523bb952b938db1b4f8974245aa0773
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-18 22:30:23 -0500

    Add skeleton api for routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const axios = require("axios");
 const apiReq = require('./apiTest'); 
 
 // apiReq();  (dont use! Limited credits)
@@ -11,9 +10,23 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//testing frontend connection
 app.get("/api", (req, res) => {
   res.json(["banana", "apple"]);
 });
+
+//route for identification 
+//BODY : [{images, organs}] up to length 4
+//RESPONSE: like sample response 
+//TO DO: Figure out how to convert apiTest to this route
+app.post("api/identify", (req, res) => {
+   
+})
+
+//route for storing images in db? 
+app.post("api/store", (req, res) => {
+
+})
 
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at http://localhost:${PORT}`);
