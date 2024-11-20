099caf876408f5c58fe6546793fed05467c77938
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-19 21:41:19 -0500

    Update Upload component & include event handlers for uploading image, type selection and submit

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,4 +1,25 @@
+import { useState, ChangeEvent } from "react";
+import { Organ } from "../types";
+
 function Upload() {
+  const [img, setImg] = useState("");
+  const [type, setType] = useState("");
+  const [organObj, setOrganObj] = useState<Organ[]>([]); 
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files) return;
+    const file = e.target.files[0]; 
+    setImg(URL.createObjectURL(file));
+  }
+
+  function handleSelect(e: ChangeEvent<HTMLSelectElement>) {
+    setType(e.target.selectedOptions[0].value);
+  }
+
+  function handleSubmit() {
+    setOrganObj([...organObj, { image: img, organ: type }]); 
+  }
+
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <label
@@ -28,7 +49,13 @@ function Upload() {
             PNG, JPG, MAX:50MB
           </p>
         </div>
-        <input id="dropzone-file" type="file" className="hidden" />
+        <input
+          id="dropzone-file"
+          type="file"
+          className="hidden"
+          onChange={handleChange}
+        />
+        <img src={img} alt="" />
       </label>
       <label
         htmlFor="organSelect"
@@ -37,8 +64,9 @@ function Upload() {
         Select an option
       </label>
       <select
+        onChange={handleSelect}
         id="organSelect"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       >
         <option selected>Choose an organ</option>
         <option value="leaf">LEAF</option>
@@ -46,8 +74,26 @@ function Upload() {
         <option value="flower">FLOWER</option>
         <option value="fruit">FRUIT</option>
       </select>
+      <button onClick={handleSubmit}
+        type="button"
+        className="py-2.5 px-5 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 w-1/2 mx-auto"
+      >
+        UPLOAD ORGAN
+      </button>
     </div>
   );
 }
 
 export default Upload;
+
+
+//Form 
+//drop zone  & select
+//add organ 
+//? Identify
+
+// STEPS
+// 1. Create an array of organs (data object) to be submitted during post 
+// 2. Write a post to the server using useEffect() 
+// 3. Posting data to our external api properly
+// 4. Confirm that we get a response of 200 from the external API
