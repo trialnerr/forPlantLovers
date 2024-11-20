7c0bf12d958ca254d7408883ad8bbfeb35d85c90
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-19 21:41:46 -0500

    changed from multiple Upload inputs to one.

--- src/pages/Identify.tsx
+++ src/pages/Identify.tsx
@@ -6,16 +6,13 @@ function Identify() {
       <div className="mx-auto max-w-3xl">
         <div className="text-center flex flex-col h-full">
           <h1 className="text-balance text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
-            Identify Plants Around You..
+            Identify Plants Around You
           </h1>
           <h2 className="text-balance text-xl tracking-tight text-gray-900 sm:text-2xl py-1">
-            Upload a clear and high quality picture below.
+            Provide one of up to four organs below
           </h2>
           <form className="my-12 flex flex-col gap-8">
             <div className=" flex gap-8">
-              <Upload />
-              <Upload />
-              <Upload />
               <Upload />
             </div>
             <button type="button" className="py-2.5 px-5 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 w-1/2 mx-auto">IDENTIFY</button>
