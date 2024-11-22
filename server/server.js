6deb9b8fcf695d217b012c2cfde032f5514bc83f
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-21 22:43:37 -0500

    Serve static files from dist folder and add catch all route that will serve index.html for other unspecified routes so that React Router does the client side routing

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const apiReq = require('./apiTest'); 
+const apiReq = require("./apiTest");
 
 // apiReq();  (dont use! Limited credits)
 
@@ -10,25 +10,27 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//serve static files from the dist folder during production
+app.use(express.static(path.join(__dirname, "../dist")));
+
 //testing frontend connection
 app.get("/api", (req, res) => {
   res.json(["banana", "apple"]);
 });
 
-//route for identification 
+//route for identification
 //BODY : [{images, organs}] up to length 4
-//RESPONSE: like sample response 
+//RESPONSE: like sample response
 //TO DO: Figure out how to convert apiTest to this route
-app.post("api/identify", (req, res) => {
-   
-})
+app.post("api/identify", (req, res) => {});
 
-//route for storing images in db? 
-app.post("api/store", (req, res) => {
+//route for storing images in db?
+app.post("api/store", (req, res) => {});
 
-})
-
+// Catch-all route to serve the React app for any unmatched routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
-
