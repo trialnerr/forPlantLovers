419cd92e794bb614c43e5cde3f14f96616560637
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-25 17:53:04 -0500

    Clean up prop types. Add UploadProps, ModalProps, OrganListItemProps

--- src/types.ts
+++ src/types.ts
@@ -3,41 +3,25 @@ export type FeatureProps = {
   heading: string;
 };
 
-export type PlantProps = {
-  common_name: string; 
-  image_url: string; 
-};
-
 export type Organ = {
-  image: string; 
+  image: File; 
   organType: string;
 }
-
-export type OrganID = { [id: string]: Organ }
 
 export type OrganType = 'fruit' | 'flower' | 'bark' | 'leaf'; 
 
 export type UploadProps = {
-  setOrganId: React.Dispatch<React.SetStateAction<OrganID[]>>;
-  currImage: string; 
-  setCurrImage: React.Dispatch<React.SetStateAction<string>>;
-  modalOpen: boolean;
-  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  handleImageUpload: (imgFile : File) => void;
 };
 
-export type ModalProps = UploadProps & {
-  organType: OrganType; 
-  id: string; 
+export type ModalProps =  {
+  currImage: File; 
+  addOrgan: (organType: OrganType) => void; 
 };
-
-export type OrganChoiceModalProps = {
-  image_url: string;
-  values: Organ[];
-  handleOrganChoice: () => void;
-}
 
 export type OrganListItemProps = {
-  value: Organ;
-  handleOrganChoice: () => void;
+  addOrgan: (organType: OrganType) => void;
+  organType: OrganType
 };
+
 
