ae70ccb57914b5f9d93aa36e5e50eeed5e92d3ac
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-25 15:45:26 -0500

    updated types

--- src/types.ts
+++ src/types.ts
@@ -8,16 +8,36 @@ export type PlantProps = {
   image_url: string; 
 };
 
-export type OrganAndImage = {
-  image: string, 
-  organ: string
+export type Organ = {
+  image: string; 
+  organType: string;
 }
 
-export type Organ = 'fruit' | 'flower' | 'bark' | 'leaf'; 
+export type OrganID = { [id: string]: Organ }
 
-export type ModalProps = {
-  isOpen: boolean;
-  hasCloseBtn?: boolean;
-  onClose?: () => void;
-  children: React.ReactNode;
+export type OrganType = 'fruit' | 'flower' | 'bark' | 'leaf'; 
+
+export type UploadProps = {
+  setOrganId: React.Dispatch<React.SetStateAction<OrganID[]>>;
+  currImage: string; 
+  setCurrImage: React.Dispatch<React.SetStateAction<string>>;
+  modalOpen: boolean;
+  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
+
+export type ModalProps = UploadProps & {
+  organType: OrganType; 
+  id: string; 
+};
+
+export type OrganChoiceModalProps = {
+  image_url: string;
+  values: Organ[];
+  handleOrganChoice: () => void;
+}
+
+export type OrganListItemProps = {
+  value: Organ;
+  handleOrganChoice: () => void;
+};
+
