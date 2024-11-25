5c03011e80bc01bae0220a666a6d7ef87f90efcd
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-25 17:48:32 -0500

    update OrganChoiceModal to only take in currImage and addOrgan as props

--- src/components/OrganChoiceModal.tsx
+++ src/components/OrganChoiceModal.tsx
@@ -1,12 +1,11 @@
-import { useRef } from "react";
-import OrganListItem from "./OrganListItem";
-import { OrganType, UploadProps } from "../types";
 
-function OrganChoiceModal({ setModalOpen, modalOpen, currImage, setOrganId, setCurrImage}: UploadProps): React.JSX.Element {
+import OrganListItem from "./OrganListItem";
+import { ModalProps, OrganType } from "../types";
+
+function OrganChoiceModal({ currImage, addOrgan }: ModalProps): React.JSX.Element {
   
   const organTypes: OrganType[] = ["fruit", "flower", "bark", "leaf"];
-  const id = 
-  const modalRef = useRef<HTMLDialogElement | null>(null);
+  const imgUrl = URL.createObjectURL(currImage);
   return (
     <div
       className="relative z-10"
@@ -23,18 +22,15 @@ function OrganChoiceModal({ setModalOpen, modalOpen, currImage, setOrganId, setC
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
             <div>
-              <img src={currImage} alt="" />
+              <img src={imgUrl} alt="" />
             </div>
             <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
               <ul>
-                {organTypes.map((val) => (
+                {organTypes.map((val, index) => (
                   <OrganListItem
-                    setOrganId={setOrganId}
-                    modalOpen={modalOpen}
-                    setModalOpen={setModalOpen}
-                    currImage={currImage}
-                    setCurrImage={setCurrImage}
-                    organType = {val}
+                    key={index}
+                    addOrgan={addOrgan}
+                    organType={val}
                   />
                 ))}
               </ul>
