ca1174dbe7992637a10278cba6b9bcad1bc79ed3
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-25 17:50:07 -0500

    update OrganListItem to take addOrgan function and organType as props. Add handleOrganChoice function which will pass the chosen organType to the parent.

--- src/components/OrganListItem.tsx
+++ src/components/OrganListItem.tsx
@@ -1,11 +1,17 @@
-import { Organ } from "../types";
+import { OrganListItemProps } from "../types";
 
-function OrganListItem(value: Organ, handleOrganChoice: () => void ) {
+function OrganListItem({
+  addOrgan, organType
+}: OrganListItemProps): React.JSX.Element {
+
+  function handleOrganChoice() {
+    addOrgan(organType); 
+  }
   return (
     <li className="text-gray-900 bg-white border border-gray-200 w-full">
       <button className="hover:bg-gray-100" onClick={handleOrganChoice}>
         <img src="" alt="" />
-        {value}
+        {organType}
       </button>
     </li>
   );
