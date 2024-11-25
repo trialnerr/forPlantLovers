763f8918ede4edce90ff6e9e885b0080dc01f24f
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-24 20:41:15 -0500

    Change Organ type to type OrganAndImage, add Organ and ModalProps

--- src/types.ts
+++ src/types.ts
@@ -8,7 +8,16 @@ export type PlantProps = {
   image_url: string; 
 };
 
-export type Organ = {
+export type OrganAndImage = {
   image: string, 
   organ: string
 }
+
+export type Organ = 'fruit' | 'flower' | 'bark' | 'leaf'; 
+
+export type ModalProps = {
+  isOpen: boolean;
+  hasCloseBtn?: boolean;
+  onClose?: () => void;
+  children: React.ReactNode;
+};
