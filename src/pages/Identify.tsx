30737849cb1abb7973dfe652dfc3532904ee4cd0
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-25 17:47:51 -0500

    Add addOrgan and handleImageUpload to the Identify component

--- src/pages/Identify.tsx
+++ src/pages/Identify.tsx
@@ -1,13 +1,43 @@
 import { useState } from "react";
 import Upload from "../components/Upload";
-import { OrganID } from "../types";
+import { Organ, OrganType } from "../types";
 import OrganChoiceModal from "../components/OrganChoiceModal";
 
 function Identify() {
-  const [organID, setOrganID] = useState<OrganID[]>([]); 
-  const [currImage, setCurrImage] = useState(''); 
+  const [organs, setOrgans] = useState<Organ[]>([]);
+  const [currImage, setCurrImage] = useState<File | null>(null); 
   const [openModal, setModalOpen] = useState<boolean>(false); 
-  console.log({currImage, openModal})
+
+  console.log({ currImage }); 
+  console.log(organs)
+  console.log(openModal);
+
+  function addOrgan(organType: OrganType) {
+    if (!currImage) {
+       console.error("No image selected");
+       return; 
+    }
+    setOrgans((prev) => [...prev, { image: currImage, organType }]);
+    setModalOpen(false); 
+  }
+
+   function handleImageUpload(file: File) {
+     console.log("file added");
+     console.log("looking for", file , "in", organs);
+     if (
+       !organs.find(
+         (organ) =>
+           organ.image.name === file.name && organ.image.size === file.size,
+       )
+     ) {
+       setCurrImage(file);
+     } else {
+       console.log("Image already exists", file);
+       setCurrImage(null);
+     }
+     setModalOpen(true);
+   }
+
   return (
     <main className="relative isolate px-6 pt-6 lg:px-8 min-h-[calc(100vh-2rem)] content-center">
       <div className="mx-auto max-w-3xl">
@@ -21,21 +51,16 @@ function Identify() {
           <h3 className="text-base sm:text-lg text-gray-800">
             At least one photo of leaf, flower, fruit or bark is required.{" "}
           </h3>
-          <OrganChoiceModal
-            setOrganId={setOrganID}
-            modalOpen={openModal}
-            setModalOpen={setModalOpen}
+          {openModal && currImage ? <OrganChoiceModal
             currImage={currImage}
-            setCurrImage={setCurrImage}
-          />
+            addOrgan={addOrgan}
+          />: null}
+        
           <form className="my-12 flex flex-col gap-8">
             <div className=" flex gap-8">
               <Upload
-                setOrganId={setOrganID}
-                modalOpen={openModal}
-                setCurrImage={setCurrImage}
-                setModalOpen={setModalOpen}
-                currImage={currImage}
+                handleImageUpload={handleImageUpload}
+                
               />
             </div>
             <button
@@ -52,3 +77,10 @@ function Identify() {
 }
 
 export default Identify;
+
+//add addOrgan 
+//deleteOrgan
+//add a toast that appears and disappears after a bit if an image was added twice
+//handleImageUpload then set modalOpen to true ✅
+
+// File { name: "img1.png", lastModified: 1731976657100, webkitRelativePath: "", size: 1368911, type: "image/png" }
