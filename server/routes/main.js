da5f7591068bd6b66fa9e2149eba1a8a782cdfe9
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-27 16:03:08 -0500

    Update image post route to respond with status code and data object containing results.

--- server/routes/main.js
+++ server/routes/main.js
@@ -5,30 +5,35 @@ const imageController = require("../controllers/imageController");
 const apiReq = require("../apiTest");
 const upload = multer({ dest: "uploads/" });
 
-router.post("/images", upload.array("image"), imageController.saveImage, async (req, res, next) => {
-  try {
-    const images = res.locals.images;
-    console.log(req.body); 
-    const organTypes = req.body.organTypes.split(','); 
-    apiReq(organTypes, images); 
-  } catch (error) {
-    next({log: `error in api call, ${error}`,
-    status: 500,
-    message: { err: "An error occurred" }})
-  }
-})
+router.post(
+  "/images",
+  upload.array("image"),
+  imageController.saveImage,
+  async (req, res, next) => {
+    try {
+      const images = res.locals.images;
+      const organTypes = req.body.organTypes.split(",");
+      const { status, data } = await apiReq(organTypes, images);
+      res.status(status).json(data);
+    } catch (error) {
+      next({
+        log: `error in api call, ${error}`,
+        status: 500,
+        message: { err: "An error occurred" },
+      });
+    }
+  },
+);
 
-module.exports = router; 
-
+module.exports = router;
 
 //save image to cloudinary then make call to API
 //respond with what you get from API response
 // app.post("/api/images", upload.array("image"), (req, res) => {
-//   console.log('I made it'); 
-//   console.log(req.body.organTypes.split(',')); 
+//   console.log('I made it');
+//   console.log(req.body.organTypes.split(','));
 //   console.log(req.files);
 
-
 //   res.send('Yay');
-//   //send the request to the backend 
+//   //send the request to the backend
 // })
