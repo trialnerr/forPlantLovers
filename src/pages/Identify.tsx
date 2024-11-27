c14e88bd8b4b5b461e425f98d1f8017581798013
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-26 21:46:43 -0500

    Update identify to add a handleIdentify function which will sent a post request with the data to the backend

--- src/pages/Identify.tsx
+++ src/pages/Identify.tsx
@@ -5,38 +5,58 @@ import OrganChoiceModal from "../components/OrganChoiceModal";
 
 function Identify() {
   const [organs, setOrgans] = useState<Organ[]>([]);
-  const [currImage, setCurrImage] = useState<File | null>(null); 
-  const [openModal, setModalOpen] = useState<boolean>(false); 
-
-  console.log({ currImage }); 
-  console.log(organs)
-  console.log(openModal);
+  const [currImage, setCurrImage] = useState<File | null>(null);
+  const [openModal, setModalOpen] = useState<boolean>(false);
+  const config = { headers: { "Content-Type": "multipart/form-data" } };
 
   function addOrgan(organType: OrganType) {
     if (!currImage) {
-       console.error("No image selected");
-       return; 
+      console.error("No image selected");
+      return;
     }
     setOrgans((prev) => [...prev, { image: currImage, organType }]);
-    setModalOpen(false); 
+    setModalOpen(false);
   }
 
-   function handleImageUpload(file: File) {
-     console.log("file added");
-     console.log("looking for", file , "in", organs);
-     if (
-       !organs.find(
-         (organ) =>
-           organ.image.name === file.name && organ.image.size === file.size,
-       )
-     ) {
-       setCurrImage(file);
-     } else {
-       console.log("Image already exists", file);
-       setCurrImage(null);
-     }
-     setModalOpen(true);
-   }
+  function handleImageUpload(file: File) {
+    console.log("file added");
+    console.log("looking for", file, "in", organs);
+    if (
+      !organs.find(
+        (organ) =>
+          organ.image.name === file.name && organ.image.size === file.size,
+      )
+    ) {
+      setCurrImage(file);
+    } else {
+      console.log("Image already exists", file);
+      setCurrImage(null);
+    }
+    setModalOpen(true);
+  }
+
+  async function handleIdentify(
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) {
+    e.preventDefault();
+    const formData = new FormData();
+    // formData.append("organs", organs)
+    const files = organs.map((organ) => organ.image);
+    files.forEach((file) => {
+      formData.append("image", file);
+    });
+    formData.append(
+      "organTypes",
+      organs.map((organ) => organ.organType).join(","),
+    );
+    // formData.append("image", files[0]);
+    console.log(formData);
+    // const organData = organs;
+    fetch("api/images", {
+      method: "POST",
+      body: formData,
+    });
+  }
 
   return (
     <main className="relative isolate px-6 pt-6 lg:px-8 min-h-[calc(100vh-2rem)] content-center">
@@ -51,20 +71,17 @@ function Identify() {
           <h3 className="text-base sm:text-lg text-gray-800">
             At least one photo of leaf, flower, fruit or bark is required.{" "}
           </h3>
-          {openModal && currImage ? <OrganChoiceModal
-            currImage={currImage}
-            addOrgan={addOrgan}
-          />: null}
-        
+          {openModal && currImage ? (
+            <OrganChoiceModal currImage={currImage} addOrgan={addOrgan} />
+          ) : null}
+
           <form className="my-12 flex flex-col gap-8">
             <div className=" flex gap-8">
-              <Upload
-                handleImageUpload={handleImageUpload}
-                
-              />
+              <Upload handleImageUpload={handleImageUpload} />
             </div>
             <button
               type="button"
+              onClick={handleIdentify}
               className="py-2.5 px-5 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 w-1/2 mx-auto"
             >
               IDENTIFY
@@ -78,9 +95,12 @@ function Identify() {
 
 export default Identify;
 
-//add addOrgan 
+//add addOrgan
 //deleteOrgan
 //add a toast that appears and disappears after a bit if an image was added twice
 //handleImageUpload then set modalOpen to true ✅
 
-// File { name: "img1.png", lastModified: 1731976657100, webkitRelativePath: "", size: 1368911, type: "image/png" }
+//when the user clicks the identify button
+//I want to save the pictures in a DB
+//make a request to the api to do the identification
+//display the name of the plant just to start
