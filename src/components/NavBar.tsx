164966fa6d4c8a071233b3de84adda221b8904c1
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-29 13:20:00 -0500

    change a tags into NavLink components in NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 function NavBar() {
   return (
     <nav className="bg-gray-200 shadow shadow-gray-300 w-100 px-8 md:px-auto">
@@ -21,38 +23,39 @@ function NavBar() {
         <div className="text-gray-500 order-3 w-full md:w-auto md:order-2">
           <ul className="flex font-semibold justify-between">
             <li className="md:px-4 md:py-2 text-indigo-500">
-              <a href="/">Home</a>
+              <NavLink to="/">Home</NavLink>
             </li>
             <li className="md:px-4 md:py-2 hover:text-indigo-400">
-              <a href="/identify">Identify a Plant</a>
+              <NavLink to="/identify">Identify a Plant</NavLink>
             </li>
             <li className="md:px-4 md:py-2 hover:text-indigo-400">
-              <a href="/search">Search Plants</a>
+              <NavLink to="/search">Search</NavLink>
             </li>
             <li className="md:px-4 md:py-2 hover:text-indigo-400">
-              <a href="/gallery">Plant Gallery</a>
+              <NavLink to="/gallery">Gallery</NavLink>
             </li>
             <li className="md:px-4 md:py-2 hover:text-indigo-400">
-              <a href="/about">About</a>
+              <NavLink to="/about">About</NavLink>
             </li>
           </ul>
         </div>
-        <div className="order-2 md:order-3">
-          <button className="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <span>Login</span>
-          </button>
+        <div className="order-2 md:order-3 flex gap-4 items-center">
+          <NavLink to="/login">Sign In</NavLink>
+            <NavLink to="/register"  className="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              Register
+            </NavLink>
         </div>
       </div>
     </nav>
