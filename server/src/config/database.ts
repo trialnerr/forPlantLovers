a232d2a2046e4dfc70b67ac00569e05fc087de96
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-30 16:44:20 -0500

    Add connectDB method in database.ts

---
+++ server/src/config/database.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_STRING);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error('Error connecting to db', err);
+    process.exit(1);
+  }
+};
+
+
