b2b8f6add90fba982a762016b4b52338613cb7cd
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-30 13:44:32 -0500

    Start from scratch add typescript to backend.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,4 @@
-import express, { Request, Response } from "express";
-import path from "path";
-import mainRouter from "./routes/main";
+import express, {  } from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,36 +7,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //serve static files from the dist folder during production
-app.use(express.static(path.join(__dirname, "../dist")));
+// app.use(express.static(path.join(__dirname, "../../../dist")));
 
-app.use("/api", mainRouter);
+// app.use("/api", mainRouter);
 
-//route for identification
-//BODY : [{images, organs}] up to length 4
-//RESPONSE: like sample response
-//TO DO: Figure out how to convert apiTest to this route
-//save the image to cloudinary first & then
-app.post("api/identify", (req, res) => {});
-
-//route for storing images in db?
-app.post("api/store", (req, res) => {});
 
 // Catch-all route to serve the React app for any unmatched routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
-});
+// app.get("*", (_, res) => {
+//   res.sendFile(path.join(__dirname, "../dist/index.html"));
+// });
 
-//global error handler
-app.use((err, req, res, next) => {
-  const defaultErr = {
-    log: "Express error handler caught unknown middleware error",
-    status: 500,
-    message: { err: "An error occurred" },
-  };
-  const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
-});
+// const globalErrorHandler: ErrorRequestHandler= (error: ServerError, _req: Request, res: Response, _next: NextFunction) => {
+//   const defaultErr: ServerError = {
+//     log: "Express error handler caught unknown middleware error",
+//     status: HttpCode.INTERNAL_SERVER_ERROR,
+//     message: "An error occured",
+//   };
+//   const errorObj = Object.assign({}, defaultErr, error);
+//   console.log(errorObj.log);
+//   res.status(errorObj.status).json(errorObj.message);
+// }
+
+// //global error handler
+// app.use(globalErrorHandler);
 
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at http://localhost:${PORT}`);
