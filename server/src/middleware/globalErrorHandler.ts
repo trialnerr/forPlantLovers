1081d51830c0b05a67359ead230011aa351e52c7
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-30 20:20:34 -0500

    Add global Error handler function

---
+++ server/src/middleware/globalErrorHandler.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import { ServerError } from "../types/types";
+
+const globalErrorHandler = (err: ServerError, req: Request, res: Response, _next: NextFunction) => {
+  if (err.status) {
+    res.status(err.status).json({ msg: err.message });
+  } else {
+    res.status(500).json({ msg: err.message });
+  }
+  console.log(err.log);
+};
+
+export default globalErrorHandler;
