e45c81057f4bfc96d63f8606b0b065220cabc85a
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-01 12:09:28 -0500

    Run connectDB function and add cookieParser as middleware.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,21 @@
 import express from "express";
-
+import { connectDB } from "./config/database";
 //error handler middleware
-import globalErrorHandler from "./middleware/globalErrorHandler";
+import  globalErrorHandler from "./middleware/globalErrorHandler";
 import notFoundHandler from "./middleware/notFound";
 //Routers
 import userRouter from "./routes/userRouter";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+await connectDB(); 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cookieParser());
 
 //serve static files from the dist folder during production
 // app.use(express.static(path.join(__dirname, "../../../dist")));
