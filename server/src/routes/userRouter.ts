b1903cd8f631af567865ec520ed940bbbb48d551
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-01 13:05:37 -0500

    Add post to login route in userRouter

--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -1,18 +1,20 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import userController from "../controllers/userController";
 import validateUserRegistration from "../middleware/validateUser";
 
 const userRouter = express.Router();
 
-// userRouter.post("/login", userController.verifyUser, (req, res) => {
-//   // post request for logging in
-// });
+
 
 // userRouter.get("/register", (req, res) => {
 //   // load structure of register page
 // });
 
-userRouter.post("/register", validateUserRegistration, userController.createUser, (req, res) => {
+userRouter.post("/register", validateUserRegistration, userController.createUser, (_req: Request, res: Response) => {
+  res.status(200).json(res.locals.user);
+});
+
+userRouter.post("/login", validateUserRegistration, userController.verifyUser, (req, res) => {
   res.status(200).json(res.locals.user);
 });
 
