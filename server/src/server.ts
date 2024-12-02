7bd635e6c8c1dae8f75f7b3904dcad69f66c6551
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-02 14:47:15 -0500

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,47 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import express from "express";
+import logger from "morgan";
+import passport from "passport";
+import { PassportStatic } from "passport";
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import cookieParser from "cookie-parser";
 import { connectDB } from "./config/database";
 //error handler middleware
 import  globalErrorHandler from "./middleware/globalErrorHandler";
 import notFoundHandler from "./middleware/notFound";
 //Routers
 import userRouter from "./routes/userRouter";
-import cookieParser from "cookie-parser";
+import { env } from "process";
+import passportConfig from "./config/passport";
 
+const typedPassport = passport as PassportStatic;
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 await connectDB(); 
+passportConfig(typedPassport);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(logger("dev"));
+
+//set up sessions 
+app.use(session({
+  secret: env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: env.DB_STRING })
+}));
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use((passport).initialize());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use((passport).session());
+
+
+
 
 //serve static files from the dist folder during production
 // app.use(express.static(path.join(__dirname, "../../../dist")));
