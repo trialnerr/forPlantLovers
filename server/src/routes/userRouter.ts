e47ebbd8763cd4c12e3d2dcc6212debd8963f63f
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-01 19:24:05 -0500

    Add route for posting to /logout in userRouter.

--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -4,18 +4,33 @@ import validateUserRegistration from "../middleware/validateUser";
 
 const userRouter = express.Router();
 
+userRouter.post(
+  "/logout",
+  userController.logoutUser,
+  (_req: Request, res: Response) => {
+    res.status(200).json({ message: "Logged out successfully" });
+  },
+);
 
+userRouter.post(
+  "/register",
+  validateUserRegistration,
+  userController.createUser,
+  (_req: Request, res: Response) => {
+    res.status(200).json(res.locals.user);
+  },
+);
 
-// userRouter.get("/register", (req, res) => {
-//   // load structure of register page
-// });
+userRouter.post(
+  "/login",
+  validateUserRegistration,
+  userController.verifyUser,
+  (req: Request, res: Response) => {
+    res.status(200).json(res.locals.user);
+  },
+);
 
-userRouter.post("/register", validateUserRegistration, userController.createUser, (_req: Request, res: Response) => {
-  res.status(200).json(res.locals.user);
-});
-
-userRouter.post("/login", validateUserRegistration, userController.verifyUser, (req, res) => {
-  res.status(200).json(res.locals.user);
-});
+//route for refreshing tokens
+//route for logout
 
 export default userRouter;
