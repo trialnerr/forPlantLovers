938f5c654c126ebd2e9a6303bb4a853d92ef7808
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-01 19:23:32 -0500

    Remove console logs and clear out eslint errors in validateUser.ts

--- server/src/middleware/validateUser.ts
+++ server/src/middleware/validateUser.ts
@@ -1,25 +1,23 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NextFunction, Request, Response } from "express";
 import validator from "validator";
 import { HttpCode, UserRegistrationRequestBody } from "../types/types";
 import { createServerError } from "../utils/createServerError";
 
 const validateUserRegistration = (
-  req: Request<object, object, UserRegistrationRequestBody >,
-  res: Response,
+  req: Request<object, object, UserRegistrationRequestBody>,
+  _res: Response,
   next: NextFunction,
 ): void => {
   const validationErrors: { msg: string }[] = [];
-  console.log(req.body, 'in validate');
-  const { email, password }: UserRegistrationRequestBody =
-    req.body;
-  console.log(email, password);
-
+  const { email, password }: UserRegistrationRequestBody = req.body;
+  
   if (!validator.isEmail(email)) {
     validationErrors.push({ msg: "Please enter a valid email address." });
   }
+
 
   if (!validator.isLength(password, { min: 8 })) {
     validationErrors.push({
@@ -27,16 +25,26 @@ const validateUserRegistration = (
     });
   }
 
-  if (validationErrors.length) {
-    const message: string = JSON.stringify(validationErrors);
-    const error = createServerError(message, HttpCode.BAD_REQUEST, 'user info failed validation'); 
-    return next(error); 
-  }
-
-  req.body.email = validator.normalizeEmail(email, {
+  const normalizedEmail: string | boolean = validator.normalizeEmail(email, {
     gmail_remove_dots: false,
   });
-  console.log(req.body, 'after manipulation');
+
+  if (!normalizedEmail) {
+    validationErrors.push({ msg: "Please enter a valid email address." });
+  } else {
+    req.body.email = normalizedEmail;
+  }
+
+  if (validationErrors.length) {
+    const message: string = JSON.stringify(validationErrors);
+    const error = createServerError(
+      message,
+      HttpCode.BAD_REQUEST,
+      "user info failed validation",
+    );
+    return next(error);
+  }
+
   return next();
 };
 
