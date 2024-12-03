210eb36a3a85631eb26f759d09bc2ad01737f73b
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-03 17:08:07 -0500

    Update userRouter to handle ff routes: /register, /login, /logout and /check

--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -1,36 +1,63 @@
 import express, { Request, Response } from "express";
 import userController from "../controllers/userController";
 import validateUserRegistration from "../middleware/validateUser";
-
+import { ensureAuth } from "../middleware/auth";
+import { PassportRequest } from "../types/types";
 const userRouter = express.Router();
-
-userRouter.post(
-  "/logout",
-  userController.logoutUser,
-  (_req: Request, res: Response) => {
-    res.status(200).json({ message: "Logged out successfully" });
-  },
-);
 
 userRouter.post(
   "/register",
   validateUserRegistration,
   userController.createUser,
-  (_req: Request, res: Response) => {
-    res.status(200).json(res.locals.user);
+  (req: Request, res: Response) => {
+    const passportReq = req as PassportRequest;
+    const { email, _id } = passportReq.user;
+    res.status(200).json({ email, _id });
   },
 );
 
 userRouter.post(
   "/login",
   validateUserRegistration,
-  userController.verifyUser,
+  userController.authenticateUser,
   (req: Request, res: Response) => {
-    res.status(200).json(res.locals.user);
+    const passportReq = req as PassportRequest;
+    const { email, _id } = passportReq.user;
+    res.status(200).json({ email, _id });
   },
 );
 
-//route for refreshing tokens
-//route for logout
+userRouter.post(
+  "/logout",
+  ensureAuth,
+  userController.logoutUser,
+  (_req: Request, res: Response) => {
+    res.status(200).json("User successfully logged out!");
+  },
+);
+
+userRouter.get("/check", ensureAuth, (req: Request, res: Response) => {
+  const passportReq = req as PassportRequest;
+  const { email, _id } = passportReq.user;
+  res.status(200).json({ email, _id });
+});
+// userRouter.get(
+//   "/protected",
+//   (req: Request, res: Response) => {
+//     if (!req.isAuthenticated()) {
+//       res.status(401).send("Unauthorized");
+//     }
+//     else {
+//       res.json(req.user);
+//     }
+//   },
+// );
+
+// userRouter.get("/protected", (req: Request, res: Response) => {
+//   if (!req.isAuthenticated()) {
+//     return res.status(401).send("Unauthorized");
+//   }
+//   res.send("Welcome to the protected route!");
+// });
 
 export default userRouter;
