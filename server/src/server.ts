f9877e1762f32488809132ed28285bbe54202191
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-03 18:37:35 -0500

    Add cors middleware, and a middleware that logs the session and session ID.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,47 +1,55 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import express from "express";
 import logger from "morgan";
 import passport from "passport";
-import { PassportStatic } from "passport";
-import session from 'express-session';
-import MongoStore from 'connect-mongo';
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./config/database";
 //error handler middleware
-import  globalErrorHandler from "./middleware/globalErrorHandler";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 import notFoundHandler from "./middleware/notFound";
 //Routers
 import userRouter from "./routes/userRouter";
 import { env } from "process";
 import passportConfig from "./config/passport";
+import cors from "cors";
+// import mainRouter from './routes/main';
 
-const typedPassport = passport as PassportStatic;
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-await connectDB(); 
-passportConfig(typedPassport);
+await connectDB();
+passportConfig(passport);
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(logger("dev"));
 
-//set up sessions 
-app.use(session({
-  secret: env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({ mongoUrl: env.DB_STRING })
-}));
+//set up sessions
+app.use(
+  session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: env.DB_STRING }),
+    // cookie: {
+    //   maxAge: 1000 * 60 * 60 * 24,
+    //   secure: false,  //come back to this
+    //   sameSite: "none",
+    // }
+  }),
+);
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-app.use((passport).initialize());
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-app.use((passport).session());
+app.use((req, res, next) => {
+  console.log("Session:", req.session);
+  console.log("Session ID:", req.sessionID);
+  next();
+});
 
-
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 //serve static files from the dist folder during production
 // app.use(express.static(path.join(__dirname, "../../../dist")));
