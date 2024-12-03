097fac24c92dfa2a55a6ebde5fae53f905ed8bde
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-03 18:28:59 -0500

    Add SignIn component with form handling and error messaging

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,6 +1,51 @@
+import { FormEvent, useContext, useRef, useState } from "react";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import ErrorMessage from "./ErrorMessage";
+import { AuthContext } from "../context/authContext";
+import { userData } from "../types";
+
 function SignIn() {
+  const context = useContext(AuthContext); 
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const [errMsg, setErrMsg] = useState<string>("");
+  const navigate = useNavigate();
+  const { state } = useLocation();
+
+  if (context?.user) {
+    return <Navigate to="/" replace />;
+  }
+
+  async function handleSubmit(e:FormEvent<HTMLFormElement>) {
+    e.preventDefault(); 
+    
+    const email: string | undefined = emailRef.current?.value;
+    const password: string | undefined = passwordRef.current?.value;
+    console.log(email, password);
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+      credentials: "include",
+    });
+
+     if (!response.ok) {
+       const { msg } = await response.json();
+       setErrMsg(msg);
+     } else {
+       const result: userData = await response.json(); 
+       //set the currentuser and then navigate to identify
+       context?.login(result);
+       //this is so that they are
+       //redirected to whatever page they were trying to go to first.
+       navigate(state?.path || "/identify");
+     }
+  }
   return (
-    <form className="w-full">
+    <form onSubmit={handleSubmit} className="w-full">
+      <ErrorMessage errMsg={errMsg} />
       <h1 className="text-3xl font-bold mb-8 text-gray-900">Sign In</h1>
       <div className="mb-6">
         <label
@@ -15,6 +60,8 @@ function SignIn() {
           className="w-full p-4 text-sm border rounded-lg focus:ring-blue-500 focus:border-blue-500"
           placeholder="example@example.com"
           required
+          ref={emailRef}
+          minLength={8}
         />
       </div>
       <div className="mb-4">
@@ -27,6 +74,8 @@ function SignIn() {
         <input
           type="password"
           id="password"
+          ref={passwordRef}
+          minLength={8}
           className="w-full p-4 text-sm border rounded-lg focus:ring-blue-500 focus:border-blue-500"
           required
         />
