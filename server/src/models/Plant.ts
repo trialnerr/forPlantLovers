061dfeebb1a0f5c8f4ba1c9079e9ea7f23e4c811
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-04 12:40:19 -0500

    Add Plant model.

--- server/src/models/Plant.ts
+++ server/src/models/Plant.ts
@@ -1,9 +1,12 @@
-import { Document, model, Schema } from "mongoose";
+import { Document, model, Schema, Types } from "mongoose";
 
 export interface IPlant extends Document {
   commonNames: string[];
   scientificName: string;
   genus: string;
+  postedBy: Types.ObjectId; 
+  apiImages: string[]; 
+  cloudinaryImages: string[]; 
 }
 
 const PlantSchema = new Schema<IPlant>({
@@ -19,7 +22,26 @@ const PlantSchema = new Schema<IPlant>({
     type: String, 
     required: true, 
   },
+  apiImages: {
+    type: [String], 
+    required: true, 
+  },
+  postedBy: {
+    type: Schema.Types.ObjectId, 
+    ref: "User", 
+    required: true, 
+  },
+  cloudinaryImages: {
+    type: [String], 
+    required: true, 
+  },
+  
 });
 
 export const Plant = model<IPlant>("Plant", PlantSchema);
 
+//get all plants and associated users
+//add a new plant 
+//update a plant
+//delete a plant
+//get all plants by user
