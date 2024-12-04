030ab62b895e778db8b516d5abfee1e17d84300f
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-04 18:52:28 -0500

    Add createPlant middleware in plantController.ts

--- server/src/controllers/plantController.ts
+++ server/src/controllers/plantController.ts
@@ -1,23 +1,29 @@
-// import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { createServerError } from "../utils/createServerError";
+import { CreatePlantBody, HttpCode } from "../types/types";
+import { Plant } from "../models/Plant";
 
-// const createPlant = async(
-//   req: Request<object, object, PlantRequestBody>,
-//   res: Response,
-//   next: NextFunction,
-// ) => {
-//   // try {
-//   //   const { name, description, images } = req.body;
-//   //   const plant = await Plant.create({ name, description, images });
-//   //   res.status(HttpCode.CREATED).json(plant);
-//   // } catch (error) {
-//   //   return next(
-//   //     createServerError(
-//   //       "Something went wrong",
-//   //       HttpCode.INTERNAL_SERVER_ERROR,
-//   //       `Error creating new plant, ${error}`,
-//   //     ),
-//   //   );
-//   // }
-// }
+const createPlant = async (req: Request<object, object, CreatePlantBody>, res: Response, next: NextFunction) => {
+  //save the plant to the db and sent back the id of the plant 
+  
+  try {
+    const plant = await Plant.create(req.body);
+    const plantId = plant._id; 
+    res.status(200).json({ plantId }); 
+    next();
+  } catch (error) {
+    return next(
+      createServerError(
+        "Something went wrong",
+        HttpCode.INTERNAL_SERVER_ERROR,
+        `Error creating new plant, ${error}`,
+      ),
+    );
+  }
+};
 
-// export default { createPlant };
+const plantController = {
+  createPlant
+}; 
+
+export default plantController; 
