5eb01030f8ad40255684c67d70df114076efbd00
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-04 12:40:04 -0500

    Remove unneccessary eslint disable line.

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { NextFunction, Request, Response } from "express";
 import {
   HttpCode,
