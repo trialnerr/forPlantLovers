e23de1b025c506962f3134d582d774074828b68a
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-03 22:11:38 -0500

    create saveImage function in imageController which will save the images in cloudinary and pass along url and public id.

---
+++ server/src/controllers/imageController.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import cloudinary from "../middleware/cloudinary";
+import { HttpCode } from "../types/types";
+import { createServerError } from "../utils/createServerError";
+const saveImage = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const uploadedImages = await Promise.all(
+      (req.files as { path: string }[]).map((file) =>
+        cloudinary.uploader.upload(file.path, {
+          folder: "plant_gallery",
+        }),
+      ),
+    );
+    const images = uploadedImages.map((upload) => ({
+      url: upload.secure_url,
+      public_id: upload.public_id,
+    }));
+
+    res.locals.images = images;
+    return next();
+  } catch (error) {
+    next(createServerError('Error identifying image', HttpCode.INTERNAL_SERVER_ERROR, `Error caught in saveImage middleware ${error}`));
+  }
+};
+
+export default { saveImage };
