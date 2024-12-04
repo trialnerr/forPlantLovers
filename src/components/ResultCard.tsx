fffb021a0b0636aa26868ccdc9124c565c972ae7
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-04 18:54:06 -0500

    Add scroll effect in ResultCard.

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -13,18 +13,20 @@ function ResultCard({ result, id, handleAddSpecies  }: ResultCardProps) {
       <div className="w-full p-4 border flex justify-center">
         <div className="flex gap-3 flex-wrap">
           {images.map((imgObj, i) => {
-            return <img src={imgObj.url.s} alt="" key={`resultCardImg${i}`}/>;
+            return <img src={imgObj.url.s} alt="" key={`resultCardImg${i}`} />;
           })}
         </div>
       </div>
       <div className="p-4 border border-gray-200 bg-gray-50 rounded-b-lg text-right">
-        <button
-          onClick={() => handleAddSpecies(id)}
-          type="button"
-          className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2"
-        >
-          This is the correct plant!
-        </button>
+        <a href="#plantSelectForm">
+          <button
+            onClick={() => handleAddSpecies(id)}
+            type="button"
+            className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2"
+          >
+            This is the correct plant!
+          </button>
+        </a>
       </div>
     </article>
   );
