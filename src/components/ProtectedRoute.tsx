2de5253649fd1a856c242919485c169c7ef5fbcc
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-04 11:32:20 -0500

    Add Loading component to ProtectedRoute

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren, useContext } from "react"
 import { AuthContext } from "../context/authContext"
 import { Navigate, useLocation } from "react-router-dom";
+import Loading from "./Loading";
 
 //this broke everything. It seems that on refresh. 
 function ProtectedRoute({ children }: PropsWithChildren) {
@@ -8,7 +9,7 @@ function ProtectedRoute({ children }: PropsWithChildren) {
   console.log(location.pathname, 'location'); 
   const context = useContext(AuthContext); 
   if (context?.loading) {
-    return <div>Loading...</div>;
+    return <Loading />
   }
   return context?.user ? (
     children
