bc1cc394a886d3f4cb5a2ef9d111cc9da9bbd557
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-04 22:33:07 -0500

    Add createNote middleware in plantNoteController.

---
+++ server/src/controllers/plantNoteController.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import { createServerError } from "../utils/createServerError";
+import { CreatePlantNoteBody, HttpCode } from "../types/types";
+import { PlantNote } from "../models/PlantNote";
+
+const createPlantNote = async (
+  req: Request<object, object, CreatePlantNoteBody>,
+  res: Response,
+  next: NextFunction,
+) => {
+  //save the plantNote to the db and sent back the id of the plant
+
+  try {
+    const plantNote = await PlantNote.create(req.body);
+    const plantNoteId = plantNote._id;
+    res.status(200).json({ plantNoteId });
+    next();
+  } catch (error) {
+    return next(
+      createServerError(
+        "Something went wrong",
+        HttpCode.INTERNAL_SERVER_ERROR,
+        `Error creating new plant Note, ${error}`,
+      ),
+    );
+  }
+};
+
+const plantNoteController = {
+  createPlantNote,
+};
+
+export default plantNoteController;
