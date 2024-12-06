56d22f8f003074ce83a429dff7ddaffd70038555
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-06 16:34:20 -0500

    Add functionality to delete a plant and its corresponding note in Gallery.tsx.

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -7,21 +7,28 @@ function Gallery() {
   const context = useContext(AuthContext);
   const userId = context?.user?._id;
   const [plantData, setPlantData] = useState<PlantDataWithNotes | null>(null);
-  
+
   async function fetchPlantData(userId: string) {
-    console.log(userId)
     const response = await fetch(`/api/plant/plantAndNotes/${userId}`);
     const responseData = await response.json();
-    console.log(responseData); 
-    setPlantData(responseData); 
+    setPlantData(responseData);
+  }
+
+  async function deletePlantAndNote(plantId: string, noteId: string) {
+    await fetch(`/api/plant/${plantId}`, { method: "DELETE" });
+    await fetch(`/api/plantNote/${noteId}`, { method: "DELETE" });
+
+    if (userId) {
+      fetchPlantData(userId);
+    }
   }
 
   useEffect(() => {
     if (userId) {
       fetchPlantData(userId);
     }
-  }, [userId])
-  
+  }, [userId]);
+
   return (
     <main className="relative isolate px-6 pt-6 lg:px-8 content-center min-h-[calc(100vh-2rem)] bg-gray-100 p-9 my-16">
       <div className="mx-auto max-w-6xl">
@@ -29,7 +36,13 @@ function Gallery() {
           Plant Gallery
         </h1>
         <div className=" grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-center">
-          {plantData?.userPlantsWithNotes.map((plantWithNote, i) => <GalleryCard plantWithNote={plantWithNote} key={`galleryCard${i}`}/>)}
+          {plantData?.userPlantsWithNotes.map((plantWithNote, i) => (
+            <GalleryCard
+              plantWithNote={plantWithNote}
+              key={`galleryCard${i}`}
+              deletePlantAndNote={deletePlantAndNote}
+            />
+          ))}
         </div>
       </div>
     </main>
