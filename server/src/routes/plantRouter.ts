d9045343b5bd591b187d6e4b2b8c2f23aa9796a0
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-06 16:38:52 -0500

    Add formatting in plantRouter.js

--- server/src/routes/plantRouter.ts
+++ server/src/routes/plantRouter.ts
@@ -4,12 +4,8 @@ import plantController from "../controllers/plantController";
 const plantRouter = express.Router();
 
 plantRouter.post("/create", plantController.createPlant);
-
-plantRouter.delete("/:id", plantController.deletePlant); 
-
+plantRouter.delete("/:plantId", plantController.deletePlant);
 plantRouter.get("/allPlants/:id", plantController.getAllUserPlants);
-
 plantRouter.get("/plantAndNotes/:userId", plantController.getAllPlantsAndNotes);
-
 
 export default plantRouter;
