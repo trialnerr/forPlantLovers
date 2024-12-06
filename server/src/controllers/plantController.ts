2100205a7cb66aa6e893d2c6b9b52177ff0560ed
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-06 13:04:34 -0500

    add getAllUserPlants, getAllPlantsAndNotes, and deletePlant functions.

--- server/src/controllers/plantController.ts
+++ server/src/controllers/plantController.ts
@@ -2,14 +2,17 @@ import { NextFunction, Request, Response } from "express";
 import { createServerError } from "../utils/createServerError";
 import { CreatePlantBody, HttpCode } from "../types/types";
 import { Plant } from "../models/Plant";
+import { Types } from "mongoose";
 
-const createPlant = async (req: Request<object, object, CreatePlantBody>, res: Response, next: NextFunction) => {
-  //save the plant to the db and sent back the id of the plant 
-  
+const createPlant = async (
+  req: Request<object, object, CreatePlantBody>,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const plant = await Plant.create(req.body);
-    const plantId = plant._id; 
-    res.status(200).json({ plantId }); 
+    const plantId = plant._id;
+    res.status(200).json({ plantId });
     next();
   } catch (error) {
     return next(
@@ -22,8 +25,86 @@ const createPlant = async (req: Request<object, object, CreatePlantBody>, res: R
   }
 };
 
-const plantController = {
-  createPlant
-}; 
+const getAllUserPlants = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const userId = req.params.id;
+    console.log(userId);
+    const userPlants = await Plant.find({ postedBy: userId });
+    res.status(200).json({ userPlants });
+  } catch (error) {
+    return next(
+      createServerError(
+        "Something went wrong",
+        HttpCode.INTERNAL_SERVER_ERROR,
+        `Error getting user's plants, ${error}`,
+      ),
+    );
+  }
+};
 
-export default plantController; 
+const getAllPlantsAndNotes = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const userId = req.params.userId;
+    console.log({ userId });
+    const userPlantsWithNotes = await Plant.aggregate([
+      { $match: { postedBy: new Types.ObjectId(userId) } },
+      {
+        $lookup: {
+          from: "plantnotes",
+          localField: "_id",
+          foreignField: "plantID",
+          as: "notes",
+        },
+      },
+    ]);
+    res.status(200).json({ userPlantsWithNotes });
+  } catch (error) {
+    return next(
+      createServerError(
+        "Something went wrong",
+        HttpCode.INTERNAL_SERVER_ERROR,
+        `Error getting user's plants with notes, ${error}`,
+      ),
+    );
+  }
+};
+
+const deletePlant = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { plantId } = req.params; 
+    console.log(plantId);
+    res.status(200).json({ plantId });
+    next();
+  } catch (error) {
+    return next(
+      createServerError(
+        "Something went wrong",
+        HttpCode.INTERNAL_SERVER_ERROR,
+        `Error creating new plant, ${error}`,
+      ),
+    );
+  }
+};
+const plantController = {
+  createPlant,
+  getAllUserPlants,
+  getAllPlantsAndNotes,
+  deletePlant
+};
+
+export default plantController;
+
+
+// https://studio3t.com/knowledge-base/articles/mongodb-aggregation-framework/#mongodb-lookup
