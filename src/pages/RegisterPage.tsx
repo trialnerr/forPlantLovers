21c64bd504de22ee9eaf81e8a058eae213c4a0b8
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-06 13:37:21 -0500

    Add styling changes to SignIn and Register page.

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,15 +1,15 @@
 import Register from "../components/Register";
-import plantImage from "../client/assets/pexels-surender-singh-561453-1317013.jpg";
+import plantImage from "../client/assets/pexels-pixabay-35196.jpg"
 
 function RegisterPage() {
   return (
-    <main className="relative isolate px-6 pt-6 lg:px-8 min-h-[100vh] flex items-center">
-      <section className="flex w-full h-full items-center justify-center">
-        <div className="flex w-[90%] max-w-7xl shadow-lg bg-white overflow-hidden rounded-lg h-[90vh]">
+    <main className="relative isolate px-6 pt-6 lg:px-8 flex justify-center items-center min-h-screen">
+      <section className="flex w-4/5 h-full items-center justify-center">
+        <div className="flex w-[90%] max-w-5xl shadow-lg bg-white overflow-hidden rounded-lg">
           <div className="w-2/5 p-8 bg-gray-50 flex items-center">
             <Register />
           </div>
-          <div className="w-3/5 h-full">
+          <div className="w-3/5">
             <img
               className="w-full h-full object-cover"
               src={plantImage}
--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,15 +1,15 @@
 import SignIn from "../components/SignIn";
-import plantImage from "../client/assets/pexels-nadiye-odabasi-3668408-24964908.jpg"
+import plantImage from "../client/assets/pexels-pixabay-60597.jpg"
 
 function SignInPage() {
   return (
-    <main className="relative isolate px-6 pt-6 lg:px-8 min-h-[100vh] flex items-center">
-      <section className="flex w-full h-full items-center justify-center">
-        <div className="flex w-[90%] max-w-7xl shadow-lg bg-white overflow-hidden rounded-lg h-[90vh]">
+    <main className="relative isolate px-6 pt-6 lg:px-8 flex justify-center items-center min-h-screen">
+      <section className="flex w-4/5 h-full items-center justify-center">
+        <div className="flex w-[90%] max-w-5xl shadow-lg bg-white overflow-hidden rounded-lg">
           <div className="w-2/5 p-8 bg-gray-50 flex items-center">
             <SignIn />
           </div>
-          <div className="w-3/5 h-full">
+          <div className="w-3/5">
             <img
               className="w-full h-full object-cover"
               src={plantImage} 
