34270a193875e1aacd25ebf923889ce4897ac9cf
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-06 16:39:33 -0500

    Add formating in plantNoteRouter.ts

--- server/src/routes/plantNoteRouter.ts
+++ server/src/routes/plantNoteRouter.ts
@@ -1,12 +1,9 @@
 import express from "express";
 import plantNoteController from "../controllers/plantNoteController";
 
-
 const plantNoteRouter = express.Router();
 
 plantNoteRouter.post("/create", plantNoteController.createPlantNote);
-
-plantNoteRouter.delete("/:noteId", plantNoteController.deletePlantNote); 
-
+plantNoteRouter.delete("/:noteId", plantNoteController.deletePlantNote);
 
 export default plantNoteRouter;
