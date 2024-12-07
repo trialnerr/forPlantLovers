edd73191119fc8304d83fee294710ddbb9d6c188
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-06 22:23:19 -0500

    Add styling and format page.

--- src/components/OrganChoiceModal.tsx
+++ src/components/OrganChoiceModal.tsx
@@ -1,13 +1,14 @@
-
 import OrganListItem from "./OrganListItem";
 import { ModalProps, OrganType } from "../types";
 
-function OrganChoiceModal({ currImage, addOrgan }: ModalProps): React.JSX.Element {
-  
+function OrganChoiceModal({
+  currImage,
+  addOrgan,
+}: ModalProps): React.JSX.Element {
   const organTypes: OrganType[] = ["fruit", "flower", "bark", "leaf"];
   const imgUrl = URL.createObjectURL(currImage);
   return (
-    <div
+    <section
       className="relative z-10"
       aria-labelledby="modal-title"
       role="dialog"
@@ -18,14 +19,16 @@ function OrganChoiceModal({ currImage, addOrgan }: ModalProps): React.JSX.Elemen
         className="fixed inset-0 bg-gray-500/75 transition-opacity"
         aria-hidden="true"
       ></div>
-      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-            <div>
-              <img src={imgUrl} alt="" />
+      {/* modal  */}
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto flex items-center justify-center p-4">
+        <div className="bg-white rounded-lg shadow-xl overflow-hidden transform transition-all sm:max-w-lg w-full">
+          <h1 className="text-lg text-center p-4 border-b">Choose Organ</h1>
+          <div className="flex">
+            <div className="w-1/2">
+              <img className="w-full h-full object-cover" src={imgUrl} alt="" />
             </div>
-            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-              <ul>
+            <div className="w-1/2 bg-gray-100">
+              <ul className="">
                 {organTypes.map((val, index) => (
                   <OrganListItem
                     key={index}
@@ -38,8 +41,8 @@ function OrganChoiceModal({ currImage, addOrgan }: ModalProps): React.JSX.Elemen
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
-};
+}
 
 export default OrganChoiceModal;
