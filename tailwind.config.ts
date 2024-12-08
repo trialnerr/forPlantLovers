7df63b32f4d2c0ffca6136c3115ef343b0588d2c
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-07 23:52:44 -0500

    Rename tailwind.config.js to tailwind.config.ts and update type annotations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+ content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       scrollBehavior: {
@@ -9,5 +10,4 @@ export default {
     },
   },
   plugins: [],
-};
-
+} satisfies Config;
