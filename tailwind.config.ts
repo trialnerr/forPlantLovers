bc468d07b7898a0b83b8bdeed48907576b1e445c
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-08 13:35:00 -0500

    Add custom color palette to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,24 @@
 import type { Config } from "tailwindcss";
 
 export default {
- content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       scrollBehavior: {
         smooth: "smooth",
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        grey: "#eee",
+        red1: "#CF1259",
+        darkRed: "#F11440",
+        blush: "#E16691",
+        pink: "#F2BAC9",
+        lightLightGreen: "#A3D9A5",
+        lightGreen: "#3B7D02",
+        green: "#265818",
+        darkGreen: "#173224",
       },
     },
   },
