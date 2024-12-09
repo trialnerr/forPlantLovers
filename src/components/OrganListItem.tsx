da1e221be6f053df4a7b5374932167691fd36f4d
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-09 12:50:08 -0500

    Refactor OrganListItem to import image object from utils

--- src/components/OrganListItem.tsx
+++ src/components/OrganListItem.tsx
@@ -1,16 +1,5 @@
 import { OrganListItemProps } from "../types";
-import fruitImg from "../client/assets/cherries.png";
-import flowerImg from "../client/assets/cherry-blossom.png";
-import barkImg from "../client/assets/bark.png";
-import leafImg from "../client/assets/tea-leaves.png";
-
-const imgObj = {
-  fruit: fruitImg,
-  flower: flowerImg,
-  bark: barkImg,
-  leaf: leafImg,
-};
-
+import imgObj from "../utils/imageObj";
 function OrganListItem({
   addOrgan,
   organType,
