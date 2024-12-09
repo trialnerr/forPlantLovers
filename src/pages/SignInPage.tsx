64df266d46eba16046dad978bf0b4cd54a52f47e
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-08 20:12:49 -0500

    Add styles in SignInPage with theme for app.

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -3,7 +3,7 @@ import plantImage from "../client/assets/pexels-pixabay-60597.jpg"
 
 function SignInPage() {
   return (
-    <main className="relative isolate px-6 pt-6 lg:px-8 flex justify-center items-center min-h-screen">
+    <main className="relative isolate px-6 pt-6 lg:px-8 flex justify-center items-center min-h-screen bg-grey">
       <section className="flex w-4/5 h-full items-center justify-center">
         <div className="flex w-[90%] max-w-5xl shadow-lg bg-white overflow-hidden rounded-lg">
           <div className="w-2/5 p-8 bg-gray-50 flex items-center">
