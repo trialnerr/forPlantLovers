bca4c1dc00aee9c9ebaf148d6299f06e6bba3826
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-08 20:28:41 -0500

    Make styling changes to the Footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,51 +1,19 @@
-const Footer = () => {
+function Footer() {
   return (
-    <footer className="bg-white shadow dark:bg-gray-900">
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="#"
-            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
-          >
-            <img src="" className="h-8" alt="Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              forPlantParents
-            </span>
-          </a>
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                Identify a plant
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                Search Plants
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                Plant Gallery
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2024{" "}
-          <a href="#" className="hover:underline">
-            forPlantParents
-          </a>
-          . All Rights Reserved.
-        </span>
-      </div>
+    <footer className="fixed bottom-0 left-0 z-20 w-full p-4 border-t border-white shadow-lg md:flex md:items-center md:justify-between md:p-6">
+      <span className="text-sm text-darkGreen sm:text-center mx-auto">
+        Made with ❤️ by{" "}
+        <a
+          href="https://github.com/trialnerr"
+          className="underline hover:text-lightGreen"
+          target="_blank"
+        >
+          Bongi Sibanda.
+        </a>{" "}
+      </span>
+      
     </footer>
   );
-};
+}
 
 export default Footer;
