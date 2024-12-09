c589f0b147281abf5eccfc7a65d0f1431ffdce09
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-09 12:51:06 -0500

    Add image object utility for for organType images

---
+++ src/utils/imageObj.ts
@@ -0,0 +1,13 @@
+import fruitImg from "../client/assets/cherries.png";
+import flowerImg from "../client/assets/cherry-blossom.png";
+import barkImg from "../client/assets/bark.png";
+import leafImg from "../client/assets/tea-leaves.png";
+
+const imgObj = {
+  fruit: fruitImg,
+  flower: flowerImg,
+  bark: barkImg,
+  leaf: leafImg,
+};
+
+export default imgObj; 
