c2e6c5dfabd14a656927291a86a54ab41c6fc294
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-08 20:24:20 -0500

    Add route to handle plant care route

--- server/src/routes/plantRouter.ts
+++ server/src/routes/plantRouter.ts
@@ -8,4 +8,7 @@ plantRouter.delete("/:plantId", plantController.deletePlant);
 plantRouter.get("/allPlants/:id", plantController.getAllUserPlants);
 plantRouter.get("/plantAndNotes/:userId", plantController.getAllPlantsAndNotes);
 
+//get plant care information based on id, Lets do some caching as well. Basics first 
+plantRouter.get("/care/:genus", plantController.getPlantCare); 
+
 export default plantRouter;
