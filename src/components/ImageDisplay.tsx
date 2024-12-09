9ff0a2509dcf309193090072f76423b39d222c18
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-09 12:50:01 -0500

    Enhance ImageDisplay component with organType buttons for deletion and icon display

--- src/components/ImageDisplay.tsx
+++ src/components/ImageDisplay.tsx
@@ -1,15 +1,50 @@
 import { ImageDisplayProps } from "../types";
+import imgObj from "../utils/imageObj";
 
 function ImageDisplay({imgUrl, organType}: ImageDisplayProps) {
   return (
-    <div className="flex items-center justify-center w-full">
+    <div className="flex items-center justify-center w-full relative">
       <div className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
-       {imgUrl ? ( <img
-          className="h-full w-full rounded-lg object-cover"
-          src={imgUrl}
-          alt={organType}
-        /> ) : null}
+        {imgUrl ? (
+          <img
+            className="h-full w-full rounded-lg object-cover"
+            src={imgUrl}
+            alt={organType}
+          />
+        ) : null}
       </div>
+
+      {organType && (
+        <>
+          <button className="absolute right-0 top-0">
+            <span className="inline-flex justify-center items-center size-8 rounded-full border-4 border-red-100 bg-red-200 text-red-800">
+              <svg
+                className="shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M18 6 6 18"></path>
+                <path d="m6 6 12 12"></path>
+              </svg>
+            </span>
+            <span className="sr-only">Delete choice</span>
+          </button>
+          <button className="absolute left-0 bottom-0 rounded-full size-12 bg-green shrink-0">
+            <img
+              className="p-2"
+              src={imgObj[organType]}
+              alt={`${organType} icon`}
+            />{" "}
+          </button>
+        </>
+      )}
     </div>
   );
 }
