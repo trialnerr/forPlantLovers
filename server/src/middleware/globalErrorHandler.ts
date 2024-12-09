db411c8f71b8decb63b93996201b219df8d1105c
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-09 18:15:53 -0500

    Remove unused param

--- server/src/middleware/globalErrorHandler.ts
+++ server/src/middleware/globalErrorHandler.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { ServerError } from "../types/types";
 
-const globalErrorHandler = (err: ServerError, req: Request, res: Response, _next: NextFunction) => {
+const globalErrorHandler = (err: ServerError, _req: Request, res: Response, _next: NextFunction) => {
   if (err.status) {
     res.status(err.status).json({ msg: err.message });
   } else {
