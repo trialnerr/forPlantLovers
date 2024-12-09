6a5d6bca8ff49f0c86ae61fa0c0f82eca4471d2d
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-08 20:22:11 -0500

    Add PlantCareDetailImages component for displaying images.

---
+++ src/components/PlantCareDetailImages.tsx
@@ -0,0 +1,18 @@
+import { PlantCareDetailImagesProps } from '../types';
+
+function PlantAndNote({ images }: PlantCareDetailImagesProps) {
+ 
+
+  return (
+    <div className="flex flex-col items-center gap-2.5">
+      <div className="flex gap-2.5">
+      {images.slice(0, 4).map((image, index) => (
+        <img key={index} className="w-16 h-16 rounded-full" src={image} alt={`Plant image ${index + 1}`} />
+      ))}
+      </div>
+   
+    </div>
+  );
+};
+
+export default PlantAndNote;
