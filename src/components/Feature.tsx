ebb09d92ee7c3f6e79568d35aadabafe428241e4
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-08 20:11:01 -0500

    Add styles to Feature component.

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -3,21 +3,19 @@ import { FeatureProps } from "../types";
 
 const Feature = ({ heading, text, icon}: FeatureProps) => {
   return (
-    <div className="p-4 max-w-sm text-center">
+    <div className="p-4 max-w-sm">
       <div className="flex rounded-lg h-full p-4 flex-col bg-gray-100 shadow-lg border ">
-        <div className="w-12 h-12 mx-auto mb-2 flex items-center justify-center rounded-full text-green flex-shrink-0 ">
-          <img
-            src={icon}
-            alt=""
-            className="rounded-full w-full h-full object-cover mx-auto"
-          />
+      <div className="flex items-center mb-3 h-20">
+        <div className="w-10 h-10 mr-3 inline-flex items-center justify-center rounded-full text-green flex-shrink-0">
+        <img src={icon} alt="" className="rounded-full w-full h-full" />
         </div>
-        <div className="flex items-center mb-3 text-center">
-          <h2 className="text-gray-900 text-lg font-medium text-center">{heading}</h2>
-        </div>
-        <div className="flex flex-col justify-between flex-grow">
-          <p className="leading-relaxed text-base text-gray-700">{text}</p>
-        </div>
+        <h2 className="text-gray-900 text-lg font-medium">
+        {heading}
+        </h2>
+      </div>
+      <div className="flex flex-col justify-between flex-grow text-center">
+        <p className="text-base text-gray-700">{text}</p>
+      </div>
       </div>
     </div>
   );
