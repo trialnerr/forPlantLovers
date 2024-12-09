bf025055c5feb748f70d79613d9f96d6bdebe020
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-09 18:16:05 -0500

    Fix unused params in multer.ts

--- server/src/middleware/multer.ts
+++ server/src/middleware/multer.ts
@@ -6,15 +6,15 @@ type DestinationCallback = (error: Error | null, destination: string) => void;
 type FileNameCallback = (error: Error | null, filename: string) => void;
 //store files in upload
 const storage = multer.diskStorage({
-  destination: (req: Request, file: Express.Multer.File, cb: DestinationCallback): void => {
+  destination: (_req: Request, _file: Express.Multer.File, cb: DestinationCallback): void => {
     cb(null, path.resolve("./upload/"));
   },
-  filename: (req: Request, file: Express.Multer.File, cb: FileNameCallback): void => {
+  filename: (_req: Request, file: Express.Multer.File, cb: FileNameCallback): void => {
     cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
 
-const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback): void => {
+const fileFilter = (_req: Request, file: Express.Multer.File, cb: FileFilterCallback): void => {
   const ext = path.extname(file.originalname).toLowerCase();
   if (ext !== ".jpeg" && ext !== ".png") {
     cb(new Error("Incorrect file type"));
