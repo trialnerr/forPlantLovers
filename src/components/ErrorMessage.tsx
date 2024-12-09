611f895cbb6dee489797376251461bdd9c91249c
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-08 23:08:37 -0500

    Refactor ErrorMessage styling.

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,11 +1,9 @@
-type ErrorMessageProps = {
-  errMsg?: string;
-}
+import { ErrorMessageProps } from "../types"
 
 function ErrorMessage({errMsg}: ErrorMessageProps) {
   return (
     <section className="relative">
-      <p aria-live="assertive" className={errMsg ? "bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-2" : "hidden"}>
+      <p aria-live="assertive" className={errMsg ? "bg-red-100 border border-red-100 text-red-700 text-sm px-4 py-3 rounded-lg shadow relative mb-2" : "hidden"}>
         {errMsg}
       </p>
     </section>
