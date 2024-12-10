b8372ecac0aa268319e6b3a2a08c681954da7ea4
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-09 19:42:40 -0500

    Update API proxy target to production server

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    // proxy: {
+    //   "/api": {
+    //     target: "http://localhost:3001",
+    //     changeOrigin: true,
+    //   },
+    // },
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "https://forplantlovers-server.onrender.com",
         changeOrigin: true,
       },
     },
