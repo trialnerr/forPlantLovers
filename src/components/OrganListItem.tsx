a2854c9f56d338ca6f0133a56523bbee5de1c14a
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-10 00:02:17 -0500

    Add disable button functionality so that multiple organTypes can't be added.

--- src/components/OrganListItem.tsx
+++ src/components/OrganListItem.tsx
@@ -3,15 +3,24 @@ import imgObj from "../utils/imageObj";
 function OrganListItem({
   addOrgan,
   organType,
+  disabledOrganTypes
 }: OrganListItemProps): React.JSX.Element {
   function handleOrganChoice() {
     addOrgan(organType);
   }
+  const disabled = disabledOrganTypes.includes(organType);
   return (
-    <li className="text-gray-900 bg-white w-full text-left hover:bg-gray-100 flex items-center px-4 py-4 border-b">
+    <li
+      className={
+        !disabled
+          ? "text-gray-900 bg-white w-full text-left hover:bg-gray-100 flex items-center px-4 py-4 border-b cursor-pointer"
+          : "text-gray-900 bg-grey w-full text-left flex items-center px-4 py-4 border-b cursor-not-allowed"
+      }
+    >
       <button
         className="hover:bg-gray-100 flex items-center space-x-4"
-        onClick={handleOrganChoice}
+        onClick={!disabled ? handleOrganChoice : undefined}
+        disabled={disabled}
       >
         <img
           src={imgObj[organType]}
