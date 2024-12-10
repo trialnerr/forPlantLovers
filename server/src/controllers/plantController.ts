fb13d12a3d895bf724f4cdaf4637b21368613f94
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-10 02:09:52 -0500

    Refactor getPlantCare to utilize utility functions for species ID and care details retrieval

--- server/src/controllers/plantController.ts
+++ server/src/controllers/plantController.ts
@@ -3,13 +3,12 @@ import { createServerError } from "../utils/createServerError";
 import {
   CreatePlantBody,
   HttpCode,
-  PerenualPlantCareData,
 } from "../types/types";
 import { Plant } from "../models/Plant";
 import { Types } from "mongoose";
-import { env } from "process";
-import { MongoDBCacheStore } from "../middleware/MongoDBCacheStore";
-const store = new MongoDBCacheStore(env.DB_STRING, "test", "cache");
+import getPlantCareDetails from "../utils/getPlantCareDetails";
+import getPlantSpeciesId from "../utils/getPlantSpecies";
+import getPlantDetails from "../utils/getPlantDetails";
 
 const createPlant = async (
   req: Request<object, object, CreatePlantBody>,
@@ -106,50 +105,22 @@ const getPlantCare = async (
   next: NextFunction,
 ) => {
   try {
-    let { genus } = req.params;
-    //query the db for the plant
-    //get the genus of the plant
-    //query the perenal api for the speciesID
-    //using the speciesID get plant care tips
-    //send back a response.
-    // const plant = await Plant.find({ _id: plantId });
-    // const genus = plant[0].genus;
-    console.log({ genus }, "before");
-    let splitGenus = genus.split(" ");
-    if (splitGenus.includes("×")) {
-      const indexOfX = splitGenus.indexOf("×");
-      splitGenus = splitGenus.slice(0, indexOfX);
-      genus = splitGenus.join(" ");
-    }
-    console.log({ genus }, "after");
-    const speciesListApiUrl = `https://perenual.com/api/species-list?key=${env.PERENUAL_API_KEY}&page=1&q=${genus}`;
-
-    const response = await store.find(speciesListApiUrl);
-    console.log(response, 'response');
-    let apiSpeciesId: number | undefined;
-
-    if (response) {
-      apiSpeciesId = response[0].id;
-    }
+    const { genus } = req.params;
+    const apiSpeciesId = await getPlantSpeciesId(genus); 
+  
     if (apiSpeciesId) {
-      console.log({ apiSpeciesId });
-      const plantCareApiUrl = `https://perenual.com/api/species-care-guide-list?key=${env.PERENUAL_API_KEY}&species_id=${apiSpeciesId}&page=1`;
-      const plantCareRes = (await store.find(
-        plantCareApiUrl,
-      )) as PerenualPlantCareData[];
-      const plantCare = plantCareRes[0].section;
+      const plantDetails = await getPlantDetails(apiSpeciesId);
+      console.log(plantDetails, "plantDetails");
+      const plantCareRes = await getPlantCareDetails(apiSpeciesId); 
+      const plantCare = plantCareRes[0].section; 
       console.log(plantCare, "plantCare");
-      res.status(200).json({ plantCare });
+      res.status(200).json({ plantCare, plantDetails }); 
     } else {
       res.status(404).json({ message: "Species ID not found" });
     }
   } catch (error) {
     return next(
-      // createServerError(
-      //   "Something went wrong",
-      //   HttpCode.INTERNAL_SERVER_ERROR,
-      //   `Error getting plantCare details, ${error}`,
-      // ),
+    
       error,
     );
   }
