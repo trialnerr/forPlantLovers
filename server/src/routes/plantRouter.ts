04c5de36bdea98e8c64d6e22442db5947c7a3c19
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-10 12:13:01 -0500

    Remove comment and add details route.

--- server/src/routes/plantRouter.ts
+++ server/src/routes/plantRouter.ts
@@ -7,8 +7,7 @@ plantRouter.post("/create", plantController.createPlant);
 plantRouter.delete("/:plantId", plantController.deletePlant);
 plantRouter.get("/allPlants/:id", plantController.getAllUserPlants);
 plantRouter.get("/plantAndNotes/:userId", plantController.getAllPlantsAndNotes);
-
-//get plant care information based on id, Lets do some caching as well. Basics first 
 plantRouter.get("/care/:genus", plantController.getPlantCare); 
+plantRouter.get("/details/:genus", plantController.getMorePlantDetails);
 
 export default plantRouter;
