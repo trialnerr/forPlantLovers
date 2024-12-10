36b7e98bd7b422437a6b6b1d4b242c8de606f7e3
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-10 13:17:17 -0500

    Remove comments and clean up imports.

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,20 +4,17 @@ import passport from "passport";
 import session from "express-session";
 import MongoStore from "connect-mongo";
 import cookieParser from "cookie-parser";
-import userRouter from "./routes/userRouter";
 import connectDB from "./config/database";
 //error handler middleware
 import globalErrorHandler from "./middleware/globalErrorHandler";
 import notFoundHandler from "./middleware/notFound";
-//Routers
-
 import { env } from "process";
 import passportConfig from "./config/passport";
 import cors from "cors";
+import userRouter from "./routes/userRouter";
 import mainRouter from "./routes/mainRouter";
 import plantRouter from "./routes/plantRouter";
 import plantNoteRouter from "./routes/plantNoteRouter";
-// import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -40,11 +37,6 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: env.DB_STRING }),
-    // cookie: {
-    //   maxAge: 1000 * 60 * 60 * 24,
-    //   secure: false,  //come back to this
-    //   sameSite: "none",
-    // }
   }),
 );
 
@@ -54,10 +46,6 @@ app.use("/api/plantNote", plantNoteRouter);
 app.use("/api/plant", plantRouter);
 app.use("/api/user", userRouter);
 app.use("/api", mainRouter);
-
-// serve static files from the dist folder during production
-// app.use(express.static(path.join(__dirname, "../../../dist")));
-
 app.use("api/*", notFoundHandler);
 // Catch-all route to serve the React app for any unmatched routes
 // app.get("*", (_, res) => {
