15c6b9ca5e860e37ed844870eefa0eca7dc1246b
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-09 22:41:36 -0500

    Update the incorrect svg props.

--- src/components/ImageDisplay.tsx
+++ src/components/ImageDisplay.tsx
@@ -34,9 +34,9 @@ function ImageDisplay({ imgUrl, organType, handleOrganDelete, index }: ImageDisp
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <path d="M18 6 6 18"></path>
                 <path d="m6 6 12 12"></path>
