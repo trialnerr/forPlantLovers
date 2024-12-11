547fd4ca7b068e0d0187fc3fa0d18d86dd04f181
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-11 00:00:30 -0500

    Refactor SignInPage layout for improved responsivene

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -3,16 +3,16 @@ import plantImage from "../client/assets/pexels-pixabay-60597.jpg"
 
 function SignInPage() {
   return (
-    <main className="relative isolate px-6 pt-6 lg:px-8 flex justify-center items-center min-h-screen bg-grey">
-      <section className="flex w-4/5 h-full items-center justify-center">
-        <div className="flex w-[90%] max-w-5xl shadow-lg bg-white overflow-hidden rounded-lg">
-          <div className="w-2/5 p-8 bg-gray-50 flex items-center">
+    <main className="relative isolate px-6 pt-6 lg:px-8 flex justify-center items-center md:min-h-screen my-24 md:my-0 ">
+      <section className="flex flex-col md:flex-row w-4/5 h-full items-center justify-center">
+        <div className="flex flex-col md:flex-row w-full sm:w-[90%] max-w-6xl sm:max-w-5xl shadow-lg bg-white overflow-hidden rounded-lg">
+          <div className="md:w-2/5 p-8 bg-gray-50 flex items-center">
             <SignIn />
           </div>
-          <div className="w-3/5">
+          <div className="md:w-3/5 hidden md:block">
             <img
               className="w-full h-full object-cover"
-              src={plantImage} 
+              src={plantImage}
               alt="Beautiful plant"
             />
           </div>
