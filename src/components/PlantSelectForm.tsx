7a5f96b5a3bde5cf89de269fcfcbe0ecdd9ce348
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-10 23:16:43 -0500

    Update placeholder in PlantSelectForm to 'Plant Common Name'

--- src/components/PlantSelectForm.tsx
+++ src/components/PlantSelectForm.tsx
@@ -84,7 +84,7 @@ function PlantSelectForm({
                 name="species"
                 id="species"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                placeholder="Type product name"
+                placeholder="Plant Common Name"
                 defaultValue={commonName}
                 readOnly={true}
                 required
