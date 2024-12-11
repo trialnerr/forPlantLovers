543d04542ee5be8955b1d5309d5146614374a03a
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-11 12:29:26 -0500

    Refactor SignInPage layout for improved alignment and responsiveness

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -3,7 +3,7 @@ import plantImage from "../client/assets/pexels-pixabay-60597.jpg"
 
 function SignInPage() {
   return (
-    <main className="relative isolate px-6 pt-6 lg:px-8 flex justify-center items-center md:min-h-screen my-24 md:my-0 ">
+    <main className="relative isolate px-6 pt-6 lg:px-8 flex justify-center md:items-center min-h-screen my-24 md:my-0 ">
       <section className="flex flex-col md:flex-row w-4/5 h-full items-center justify-center">
         <div className="flex flex-col md:flex-row w-full sm:w-[90%] max-w-6xl sm:max-w-5xl shadow-lg bg-white overflow-hidden rounded-lg">
           <div className="md:w-2/5 p-8 bg-gray-50 flex items-center">
