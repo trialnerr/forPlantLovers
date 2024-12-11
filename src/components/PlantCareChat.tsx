66382bd788fbf1f51b69983617236a827f744523
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-12-10 21:06:42 -0500

    Add styling in PlantCare Chat component. Align the sides using margins

--- src/components/PlantCareChat.tsx
+++ src/components/PlantCareChat.tsx
@@ -21,7 +21,7 @@ function PlantCareChat({ plantCare , plantDetails}: PlantCareChatProps) {
         </h2>
       )} */}
       {plantDetails && (
-        <div className="bg-white border border-gray-200 rounded-2xl p-4 space-y-3 mb-10">
+        <div className="bg-white border border-gray-200 rounded-2xl p-4 space-y-3 mb-10 md:mx-16">
           <h2 className="font-medium text-gray-800 ">{`Care level ${plantDetails.care_level}`}</h2>
           <div className="space-y-1.5">
             <p className="mb-1.5 text-sm text-gray-800 ">
@@ -31,73 +31,74 @@ function PlantCareChat({ plantCare , plantDetails}: PlantCareChatProps) {
         </div>
       )}
       {plantCare && (
-      <ul className="space-y-5">
-        {plantCare[0] && (
-          <li className="flex gap-x-2 sm:gap-x-4 me-11">
-            <img
-              className="inline-block size-12 shrink-0 rounded-full"
-              src={imgObj[plantCare[0].type]}
-              alt="Avatar"
-            ></img>
+        <ul className="space-y-5">
+          {plantCare[0] && (
+            <li className="flex gap-x-2 sm:gap-x-4 me-11">
+              <img
+                className="inline-block size-8 md:size-12 shrink-0 rounded-full"
+                src={imgObj[plantCare[0].type]}
+                alt="Avatar"
+              ></img>
 
-            {/* <!-- Card --> */}
-            <div className="bg-white border border-gray-200 rounded-2xl p-4 space-y-3">
-              <h2 className="font-medium text-gray-800 ">
-                {plantCare[0].type}
-              </h2>
-              <div className="space-y-1.5">
-                <p className="mb-1.5 text-sm text-gray-800 ">
-                  {plantCare[0].description}
-                </p>
-              </div>
-            </div>
-            {/* -- End Card -- */}
-          </li>
-        )}
-        {plantCare[1] && (
-          <li className="flex ms-auto gap-x-2 sm:gap-x-4">
-            <div className="grow text-end space-y-3">
-              <div className="inline-block bg-white border border-gray-200 rounded-2xl p-4 shadow-sm">
+              {/* <!-- Card --> */}
+              <div className="bg-white border border-gray-200 rounded-2xl p-4 space-y-3 md:mr-5">
                 <h2 className="font-medium text-gray-800 ">
-                  {plantCare[1].type}
+                  {plantCare[0].type}
                 </h2>
                 <div className="space-y-1.5">
-                  <p className="mt-1.5 text-sm text-gray-800 ">
-                    {plantCare[1].description}
+                  <p className="mb-1.5 text-sm text-gray-800 ">
+                    {plantCare[0].description}
                   </p>
                 </div>
               </div>
-            </div>
-            <img
-              className="inline-block size-12 shrink-0 rounded-full"
-              src={imgObj[plantCare[1].type]}
-              alt="Avatar"
-            ></img>
-            {/* </span> */}
-          </li>
-        )}
-        {plantCare[2] && (
-          <li className="flex gap-x-2 sm:gap-x-4 me-11">
-            <img
-              className="inline-block size-12 shrink-0 rounded-full"
-              src={imgObj[plantCare[2].type]}
-              alt="Avatar"
-            ></img>
-
-            {/* <!-- Card --> */}
-            <div className="bg-white border border-gray-200 rounded-2xl p-4 space-y-3">
-              <h2 className="font-medium text-gray-800 ">
-                {plantCare[2].type}
-              </h2>
-              <div className="space-y-1.5">
-                <p className="mt-1.5 text-sm text-gray-800 ">
-                  {plantCare[2].description}
-                </p>
+              {/* -- End Card -- */}
+            </li>
+          )}
+          {plantCare[1] && (
+            <li className="flex ms-auto gap-x-2 sm:gap-x-4">
+              <div className="grow text-end space-y-3 ml-16">
+                <div className="inline-block bg-white border border-gray-200 rounded-2xl p-4 shadow-sm">
+                  <h2 className="font-medium text-gray-800 ">
+                    {plantCare[1].type}
+                  </h2>
+                  <div className="space-y-1.5">
+                    <p className="mt-1.5 text-sm text-gray-800 ">
+                      {plantCare[1].description}
+                    </p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </li>
-        )}
-      </ul>)}
+              <img
+                className="inline-block size-8 md:size-12 shrink-0 rounded-full"
+                src={imgObj[plantCare[1].type]}
+                alt="Avatar"
+              ></img>
+              {/* </span> */}
+            </li>
+          )}
+          {plantCare[2] && (
+            <li className="flex gap-x-2 sm:gap-x-4 me-11">
+              <img
+                className="inline-block size-8 md:size-12 shrink-0 rounded-full"
+                src={imgObj[plantCare[2].type]}
+                alt="Avatar"
+              ></img>
+
+              {/* <!-- Card --> */}
+              <div className="bg-white border border-gray-200 rounded-2xl p-4 space-y-3 md:mr-4">
+                <h2 className="font-medium text-gray-800 ">
+                  {plantCare[2].type}
+                </h2>
+                <div className="space-y-1.5">
+                  <p className="mt-1.5 text-sm text-gray-800 ">
+                    {plantCare[2].description}
+                  </p>
+                </div>
+              </div>
+            </li>
+          )}
+        </ul>
+      )}
     </section>
   );
 }
