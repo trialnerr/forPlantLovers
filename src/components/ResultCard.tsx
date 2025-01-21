76b43feaf4101d4c99798d6f158cdd9b0b14f16d
trialnerr <54381853+trialnerr@users.noreply.github.com>
2025-01-20 22:59:14 -0500

    Change result card button styles

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -36,7 +36,7 @@ function ResultCard({ result, id, handleAddSpecies  }: ResultCardProps) {
           <button
             onClick={() => handleAddSpecies(id)}
             type="button"
-            className="text-gray-900 bg-lightLightGreen hover:bg-lightGreen border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 mb-2"
+            className="text-grey bg-lightGreen hover:bg-green border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 mb-2"
           >
             This is the correct plant!
           </button>
